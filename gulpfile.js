d5d2e5f68775e722fcdcd5d9b8a29f5a6f5f5a80
Dino Abdurahmanovic <dino.abdurahmanovic@gmail.com>
2018-01-12 15:34:22 +0100

    update

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+
+
+gulp.task('sass', function(){
+  return gulp.src('sass-test/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css-test'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('sass-test/*.scss', ['sass']);
+  gulp.watch('*.html', browserSync.reload);
+  // gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+});
+
+gulp.task('imagemin', function(){
+  return gulp.src('img/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('img-test'))
+});
